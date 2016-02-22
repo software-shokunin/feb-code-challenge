ecace84a833e3b544801bef5cf8ac991547f99d2
Fiona Coath <fcoath@thoughtworks.com>
2016-02-22 23:56:02 +1100

    mmm solutiony

---
+++ 9-hardware6bit/go.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+echo "1. Go to the Brisbane ThoughtWorks office"
+echo "2. Pick up the scary circuit board thing"
+echo "3. Plug in the usb attached to the scary circuit board thing into a power source"
+echo "4. Pray nothing blows up"
+echo "5. Use the red DIL switches to input your 2 binary encoded numbers"
+echo "6. Read the binary encoded output on the LEDs in the bottom right corner"
+echo "7. ???"
+echo "8. Profit!"
